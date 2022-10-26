20f2e168636797572abc9506f41c5de1bd445239
Zach Bridges <zachery.bridges@skillshare.com>
2022-10-26 15:18:35 -0400

    initial commit: mvp

---
+++ allow-button-click.js
@@ -0,0 +1,21 @@
+function clickAllowButton() {
+    // TODO wait for something
+    document.getElementById("cli_login_button").click()
+}
+
+function isLoginSucessful() {
+    // could wait until @ awsapps.com/start/user-consent/login-success.html
+    if (document.documentElement.textContent.indexOf('Request approved') > -1) {
+        console.log("Sucess! close window");
+        
+        // window.close(); cant do this; what else?
+        // kick up an alert() to confirm so I can just hit space to close?
+    }
+}
+console.log("(｡◕‿◕｡)ﾉ ohai");
+window.addEventListener('pageshow', (event) => {
+    isLoginSucessful(); // validation 
+})
+clickAllowButton();
+
+
